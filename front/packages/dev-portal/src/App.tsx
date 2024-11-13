3ac72b722a71d4ae29dc44a65331177b99f65322
wonbin.lee <wonbin.lee@wefunding.com>
2024-11-13 18:55:47 +0900

    dev-portal react app 초기 설정

---
+++ front/packages/dev-portal/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Welcome to Dev Portal
+      </h1>
+    </div>
+  )
+}
+
+export default App
