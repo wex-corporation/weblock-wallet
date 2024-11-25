5d22bcef32334af7139e7b138c01e7b7e32f8776
wonbin.lee <wonbin.lee@wefunding.com>
2024-11-25 18:07:05 +0900

    modify dependencies scripts for deploying packages

--- weblock-wallet/scripts/revert-dependencies.js
+++ weblock-wallet/scripts/revert-dependencies.js
@@ -1,38 +1,27 @@
 const fs = require('fs')
 const path = require('path')
 
-// 대상 패키지 목록
 const packages = ['types', 'core', 'sdk']
 
-// 의존성 되돌리기 함수
 function revertDependencies(pkgDir) {
   const pkgPath = path.resolve(pkgDir, 'package.json')
   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
 
-  let isReverted = false // 변경 여부 확인
   ;['dependencies', 'devDependencies'].forEach((depType) => {
     if (pkg[depType]) {
       Object.keys(pkg[depType]).forEach((dep) => {
         if (dep.startsWith('@wefunding-dev/')) {
-          if (pkg[depType][dep] !== 'workspace:*') {
-            pkg[depType][dep] = 'workspace:*'
-            console.log(`Reverted ${dep} to workspace:* in ${pkgDir}`)
-            isReverted = true
-          }
+          pkg[depType][dep] = 'workspace:*'
+          console.log(`Reverted ${dep} to workspace:* in ${pkgDir}`)
         }
       })
     }
   })
 
-  if (isReverted) {
-    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
-    console.log(`Updated dependencies in ${pkgDir}/package.json`)
-  } else {
-    console.log(`No changes made to ${pkgDir}/package.json`)
-  }
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2))
+  console.log(`Reverted dependencies in ${pkgDir}/package.json`)
 }
 
-// 각 패키지 처리
 packages.forEach((pkg) => {
   revertDependencies(path.resolve(__dirname, '../', pkg))
 })
--- weblock-wallet/scripts/update-dependencies.js
+++ weblock-wallet/scripts/update-dependencies.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
+const { execSync } = require('child_process')
 
-// 패키지 이름과 디렉토리 이름 매핑
 const packageDirMap = {
   '@wefunding-dev/wallet-types': 'types',
   '@wefunding-dev/wallet-core': 'core',
@@ -15,48 +15,26 @@ function updateDependencies(packageDir) {
   const packageJsonPath = path.join(rootPath, packageDir, 'package.json')
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'))
 
-  let isUpdated = false // 변경 여부 확인
   const updateFields = ['dependencies', 'devDependencies']
-
   updateFields.forEach((field) => {
     if (packageJson[field]) {
       for (const [key, value] of Object.entries(packageJson[field])) {
         if (value.startsWith('workspace:*')) {
-          const dependencyDir = packageDirMap[key]
-          if (!dependencyDir) {
-            console.error(`Error: No directory mapping for ${key}`)
-            continue
-          }
-
-          const dependencyPackagePath = path.join(
-            rootPath,
-            dependencyDir,
-            'package.json'
-          )
-          const dependencyPackageJson = JSON.parse(
-            fs.readFileSync(dependencyPackagePath, 'utf-8')
-          )
-          const dependencyVersion = `^${dependencyPackageJson.version}`
-
+          const latestVersion = execSync(`npm show ${key} version`, {
+            encoding: 'utf-8'
+          }).trim()
           console.log(
-            `Updating ${key} in ${packageDir}: workspace:* -> ${dependencyVersion}`
+            `Updating ${key} in ${packageDir}: workspace:* -> ^${latestVersion}`
           )
-          packageJson[field][key] = dependencyVersion
-          isUpdated = true
+          packageJson[field][key] = `^${latestVersion}`
         }
       }
     }
   })
 
-  if (isUpdated) {
-    fs.writeFileSync(
-      packageJsonPath,
-      JSON.stringify(packageJson, null, 2) + '\n'
-    )
-    console.log(`Updated dependencies in ${packageDir}/package.json`)
-  } else {
-    console.log(`No changes made to ${packageDir}/package.json`)
-  }
+  // 변경된 package.json 저장
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n')
+  console.log(`Updated dependencies in ${packageDir}/package.json`)
 }
 
 // 각 패키지의 의존성을 업데이트
