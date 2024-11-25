e3f630ec7949c4488b2e166b53e5097229dbbc06
wonbin.lee <wonbin.lee@wefunding.com>
2024-11-25 18:31:48 +0900

    fix update-dependencies.js for versioning

--- weblock-wallet/scripts/update-dependencies.js
+++ weblock-wallet/scripts/update-dependencies.js
@@ -10,10 +10,11 @@ const packageDirMap = {
 
 const rootPath = path.resolve(__dirname, '..')
 
-function updateDependencies(packageDir) {
+function updateDependenciesAndVersion(packageDir) {
   const packageJsonPath = path.join(rootPath, packageDir, 'package.json')
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'))
 
+  // 최신 버전 의존성 업데이트
   const updateFields = ['dependencies', 'devDependencies']
   updateFields.forEach((field) => {
     if (packageJson[field]) {
@@ -31,8 +32,19 @@ function updateDependencies(packageDir) {
     }
   })
 
+  // 패키지 버전 업데이트
+  const currentVersion = packageJson.version
+  const [major, minor, patch] = currentVersion.split('.').map(Number)
+  const newVersion = `${major}.${minor}.${patch + 1}`
+  console.log(
+    `Updating ${packageDir} version: ${currentVersion} -> ${newVersion}`
+  )
+  packageJson.version = newVersion
+
+  // 변경된 package.json 저장
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n')
-  console.log(`Updated dependencies in ${packageDir}/package.json`)
+  console.log(`Updated dependencies and version in ${packageDir}/package.json`)
 }
 
-Object.values(packageDirMap).forEach(updateDependencies)
+// 각 패키지의 의존성과 버전 업데이트
+Object.values(packageDirMap).forEach(updateDependenciesAndVersion)
