d0c16df12a1a193c1a9441d0b7b1d145dce7206c
wonbin.lee <wonbin.lee@wefunding.com>
2024-11-28 18:39:37 +0900

    fix : sdk 빌드 프로세스 수정 완료

--- weblock-wallet/sdk/src/core-adapter/index.ts
+++ weblock-wallet/sdk/src/core-adapter/index.ts
@@ -4,7 +4,7 @@ export class CoreAdapter {
   private core: Core
 
   constructor(apiKey: string, env: string, orgHost?: string) {
-    this.core = new Core()
+    this.core = new Core('local', 'test-api-key', 'http://localhost:3000')
   }
 
   // TODO: Core와의 연결 메서드 추가
